52bf1eef6991c07324ccb95c31c18a38e62b397d
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-30 23:51:31 +0100

    feat: layout and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
-import "./App.css";
+import twitterLogo from "./assets/logo-twitter.svg";
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  User,
+} from "@phosphor-icons/react";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="logo" />
+
+        <nav className="main-navigation">
+          <a className="active" href="#">
+            <House /> Home
+          </a>
+          <a href="#">
+            <Hash /> Explore
+          </a>
+          <a href="#">
+            <Bell /> Notification
+          </a>
+          <a href="#">
+            <Envelope /> Messages
+          </a>
+          <a href="#">
+            <BookmarkSimple /> Bookmarks
+          </a>
+          <a href="#">
+            <FileText /> Lista
+          </a>
+          <a href="#">
+            <User /> Profile
+          </a>
+          <a href="#">
+            <DotsThreeCircle /> More
+          </a>
+        </nav>
+
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+
+      <div className="content"></div>
+    </div>
+  );
 }
 
 export default App;
