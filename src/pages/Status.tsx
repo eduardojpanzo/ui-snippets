d1091fb5fffb7416b5a4d07bbefbe16b541322f3
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-07-01 22:36:15 +0100

    feat: save data

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,16 +1,25 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
 import "./status.css";
 
-const answers = [
+const initial = [
   "Concordo...",
   "olha, faz sentido!",
   "Parabéns pelo progresso.",
 ];
 
 export default function Status() {
+  const [answers, setAnswers] = useState(initial);
+  const [newAnswer, setNewAnswer] = useState("");
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([newAnswer, ...answers]);
+    setNewAnswer("");
+  }
   return (
     <main className="status">
       <Header title="Tweet" />
@@ -18,10 +27,17 @@ export default function Status() {
       <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptates obcaecati eum voluptatibus omnis harum adipisci, perferendis quasi perspiciatis ducimus." />
       <Separator />
 
-      <form className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/eduardopanzo.png" alt="João Panzo" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newAnswer}
+            onChange={(event) => {
+              setNewAnswer(event.target.value);
+            }}
+          />
         </label>
 
         <button type="submit">Answer</button>
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,8 +1,9 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
-const tweets = [
+const initial = [
   "Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram:",
   "✅ npm start: De 32s para 400ms (sim, demorava 30s) ✅ npm build: De 120s para 22s",
   "Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥",
@@ -11,14 +12,29 @@ const tweets = [
 import "./timeline.css";
 
 export default function Timeline() {
+  const [tweets, setTweets] = useState(initial);
+  const [newTweet, setNewTweet] = useState("");
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet("");
+  }
   return (
     <main className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/eduardopanzo.png" alt="João Panzo" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            value={newTweet}
+            placeholder="What's happening?"
+            onChange={(event) => {
+              setNewTweet(event.target.value);
+            }}
+          />
         </label>
 
         <button type="submit">Tweet</button>
